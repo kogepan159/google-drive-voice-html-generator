4c965f763c2a98c3502c32bdfc01432a7c7d9f53
Junya Kengo <kogepan6852@yahoo.co.jp>
2021-05-08 20:58:11 +0900

    音声と画像を埋め込める様にする

--- index.js
+++ index.js
@@ -8,13 +8,12 @@ const add = (x,y) => {
     return "10より大きい"
   }
 
-  function OnButtonClick(url, titleName) {
+  function OnButtonClick(url, titleName, googleUrlImage) {
     target = document.getElementById("output");
     // サンプル
     // https://drive.google.com/file/d/1hKlZh7-v0wLlJU59jiPbYDbWOQu3SkwA/view?usp=sharing
     const words = url.split('/');
-    //googleUrlId = document.getElementById("googleUrl").nodeValue;
-    target.innerHTML = "https://docs.google.com/uc?id=" + words[5];
+    const googleUrlImageWords = googleUrlImage.split('/');
     targetEmbedded = document.getElementById("embedded")
-    targetEmbedded.value = `<iframe width=\"560\" height=\"315\" src=\"https://kogepan159.github.io/google-drive-voice-html-generator/music_player/music_player.html?title=${titleName}&url=https://docs.google.com/uc?id=${words[5]}\"></iframe>`
+    targetEmbedded.value = `<iframe width=\"560\" height=\"160\" src=\"https://kogepan159.github.io/google-drive-voice-html-generator/music_player/music_player.html?title=${titleName}&urlId=https://docs.google.com/uc?id=${words[5]}\&imageId=${googleUrlImageWords[5]}\"></iframe>`
   };
