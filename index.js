b5baa00d198276fdce5dfd94dc09811a22763341
Junya Kengo <kogepan6852@yahoo.co.jp>
2021-05-08 12:23:58 +0900

    タイトルを可変にする

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const add = (x,y) => {
     return "10より大きい"
   }
 
-  function OnButtonClick(mozi) {
+  function OnButtonClick(url, titleName) {
     target = document.getElementById("output");
     // サンプル
     // https://drive.google.com/file/d/1hKlZh7-v0wLlJU59jiPbYDbWOQu3SkwA/view?usp=sharing
-    const words = mozi.split('/');
+    const words = url.split('/');
     //googleUrlId = document.getElementById("googleUrl").nodeValue;
     target.innerHTML = "https://docs.google.com/uc?id=" + words[5];
+    targetEmbedded = document.getElementById("embedded")
+    targetEmbedded.value = `<iframe width=\"560\" height=\"315\" src=\"https://kogepan159.github.io/google-drive-voice-html-generator/music_player/music_player.html?title=${titleName}&url=https://docs.google.com/uc?id=${words[5]}\"></iframe>`
   };
